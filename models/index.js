a43f2f5ba2a99727b601a1efb76d0d252e476258
Cherron Simes <cherron@developdbycherron.com>
2021-09-16 20:49:13 -0500

    Added additional controller files, starting employee controller

--- models/index.js
+++ models/index.js
@@ -1,22 +1,27 @@
 const { sequelize, syncDb } = require("../db");
 const { DataTypes } = require("sequelize");
 
-
+/* Model Functions */
 const DefineUser = require('./User')
-const User = DefineUser(sequelize, DataTypes)
-
 const DefineEmployee = require("./Employee");
-const Employee = DefineEmployee(sequelize, DataTypes);
-
 const DefineCompany = require("./Company");
-const Company = DefineCompany(sequelize, DataTypes);
-
 const DefineHours = require("./Hours");
-const Hours = DefineHours(sequelize, DataTypes);
-
 const DefineProjects = require("./Projects");
+
+/* Model Definitions */
+const User = DefineUser(sequelize, DataTypes)
+const Employee = DefineEmployee(sequelize, DataTypes);
+const Company = DefineCompany(sequelize, DataTypes);
+const Hours = DefineHours(sequelize, DataTypes);
 const Projects = DefineProjects(sequelize, DataTypes);
 
+/* Associations */
+// Employee.hasOne(User)
+// User.belongsTo(Employee)
+// Company.hasMany(Employee)
+
+
+/* Sync */
 syncDb(sequelize, { alter: true });
 
 module.exports = { User, Employee, Company, Hours, Projects };
