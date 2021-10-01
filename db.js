41ae8daad8f251e34f649ca838dcf750696a7db6
Cherron Simes <cherron@developdbycherron.com>
2021-09-30 20:16:25 -0500

    debugging

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const { options } = require("./controllers/userController");
+// const { options } = require("./controllers/userController");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: "postgres",
