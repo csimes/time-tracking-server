6073fa7abc46d9c9639fa96f9eee58dbcebe78d0
Cherron Simes <cherron@developdbycherron.com>
2021-09-30 19:48:28 -0500

    debugging

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,14 @@
+const userController = require("./userController");
+const employeeController = require("./employeeController");
+const companyController = require("./companyController");
+const timesheetController = require("./timesheetController");
+const projectsController = require("./projectsController");
+
+
 module.exports = {
-    userController: require("./userController"),
-    employeeController: require("./employeeController"),
-    companyController: require("./companyController"),
-    timesheetController: require("./timesheetController"),
-    projectsController: require("./projectsController"),
+    userController,
+    employeeController,
+    companyController,
+    timesheetController,
+    projectsController
 };
