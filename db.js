69ce2506adbda203c12b4c7d83c7f7a770356f2c
Cherron Simes <cherron@developdbycherron.com>
2021-10-03 13:58:48 -0500

    Reduced logic needed in db

--- db.js
+++ db.js
@@ -2,14 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: "postgres",
-        ssl: process.env.ENVIRONMENT === "production",
-        dialectOptions: {
-          ssl: {
-            require: true, 
-            rejectUnauthorized: false
-          }
-        }
+        dialect: "postgres"
     });
 
 async function syncDb(sequelize, options) {
