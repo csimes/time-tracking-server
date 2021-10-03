e625861203f038ec3ae419393f38b477793a39d7
Cherron Simes <cherron@developdbycherron.com>
2021-10-03 12:38:25 -0500

    Added logic for server to work locally or deployed

--- db.js
+++ db.js
@@ -2,15 +2,20 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: "postgres",
-        ssl: process.env.ENVIRONMENT === "production",
-        dialectOptions: {
-          ssl: {
-            require: true, 
-            rejectUnauthorized: false
-          }
-        }
-    });
+    dialect: "postgres",
+    ssl: process.env.ENVIRONMENT === "production",
+});
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//         dialect: "postgres",
+//         ssl: process.env.ENVIRONMENT === "production",
+//         dialectOptions: {
+//           ssl: {
+//             require: true, 
+//             rejectUnauthorized: false
+//           }
+//         }
+//     });
 
 async function syncDb(sequelize, options) {
     const { force, alter } = options;
