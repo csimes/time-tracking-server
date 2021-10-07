1b36b612f96a3b9e6565679826aee6bf42263ade
Cherron Simes <cherron@developdbycherron.com>
2021-10-06 19:16:23 -0500

    Revertingserver back to previous state, using cors package didn't resolve error

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors");
 ;(async () => {
   app.use(require("./middleware/headers"));
-  app.use(
-      cors({
-          origin: "https://cs-timetrackerclient.herokuapp.com",
-          credentials: true,
-      })
-  ); 
   const controllers = require("./controllers")
   app.use(express.json());
 
