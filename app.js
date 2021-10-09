197e3e523e65a156ec7e2351882e776c5f8476cf
Cherron Simes <cherron@developdbycherron.com>
2021-10-08 19:03:44 -0500

    Working on CORS error

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 ;(async () => {
-  app.use(require("./middleware/headers"));
-  const controllers = require("./controllers")
-  app.use(express.json());
+    app.use(require("./middleware/headers"));
+    const controllers = require("./controllers");
+    app.use(express.json());
 
-  app.use("/user", controllers.userController);
-  app.use(require("./middleware/validate-session"));
-  app.use("/employee", controllers.employeeController);
-  app.use("/company", controllers.companyController);
-  app.use("/timesheet", controllers.timesheetController);
-  app.use("/project", controllers.projectsController);
+    app.use("/user", controllers.userController);
+    app.use(require("./middleware/validate-session"));
+    app.use("/employee", controllers.employeeController);
+    app.use("/company", controllers.companyController);
+    app.use("/timesheet", controllers.timesheetController);
+    app.use("/project", controllers.projectsController);
+    // Add headers before the routes are defined
 
-  app.listen(process.env.PORT, () => {
-      console.log(`[Server]: App is listening at port ${process.env.PORT}`);
-  });
+    app.listen(process.env.PORT, () => {
+        console.log(`[Server]: App is listening at port ${process.env.PORT}`);
+    });
 })();
