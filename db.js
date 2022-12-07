eeda2f1f16ea800dcb1eced74b47d061598c932c
Cherron Simes <cherron@developdbycherron.com>
2022-12-06 22:20:41 -0600

    Update database URL for use in Render

--- db.js
+++ db.js
@@ -2,34 +2,36 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-    process.env.DATABASE_URL,
-    process.env.HOST != "localhost"
-        ? {
-              dialect: "postgres",
-              dialectOptions: {
-                  ssl: {
-                      require: true,
-                      rejectUnauthorized: false,
-                  },
-              },
-          }
-        : {
-              dialect: "postgres",
-          }
+    /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */ 
+  `${process.env.DATABASE_URL}?ssl=true`,
+  process.env.HOST != "localhost"
+    ? {
+        dialect: "postgres",
+
+        //   dialectOptions: {
+        //       ssl: {
+        //           require: true,
+        //           rejectUnauthorized: false,
+        //       },
+        //   },
+      }
+    : {
+        dialect: "postgres",
+      }
 );
 
 async function syncDb(sequelize, options) {
-    const { force, alter } = options;
-    try {
-        if (force) await sequelize.sync({ force: true });
-        else if (alter) await sequelize.sync({ alter: true });
-        else await sequelize.sync();
-    } catch (err) {
-        console.log(err);
-    }
+  const { force, alter } = options;
+  try {
+    if (force) await sequelize.sync({ force: true });
+    else if (alter) await sequelize.sync({ alter: true });
+    else await sequelize.sync();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = {
-    sequelize,
-    syncDb,
+  sequelize,
+  syncDb,
 };
