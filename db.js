f0af291b94a973b22c271abce56e9d04f6cc17a5
Cherron Simes <cherron@developdbycherron.com>
2022-12-11 19:12:55 -0600

    Uncomment dialect options in db.js

--- db.js
+++ db.js
@@ -2,18 +2,18 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-    /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */ 
+  /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */
   `${process.env.DATABASE_URL}?ssl=true`,
   process.env.HOST != "localhost"
     ? {
         dialect: "postgres",
 
-        //   dialectOptions: {
-        //       ssl: {
-        //           require: true,
-        //           rejectUnauthorized: false,
-        //       },
-        //   },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
       }
     : {
         dialect: "postgres",
