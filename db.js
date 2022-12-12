3c286714c6226eafe6fae4a0e24709e87ddbadf8
Cherron Simes <cherron@developdbycherron.com>
2022-12-11 20:00:11 -0600

    Refactor db file to prevent Render delpoy error

--- db.js
+++ db.js
@@ -1,24 +1,30 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */
-  `${process.env.DATABASE_URL}?ssl=true`,
-  process.env.HOST != "localhost"
-    ? {
-        dialect: "postgres",
+// const sequelize = new Sequelize(
+//   /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */
+//   `${process.env.DATABASE_URL}?ssl=true`,
+//   process.env.HOST != "localhost"
+//     ? {
+//         dialect: "postgres",
 
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
-      }
-    : {
-        dialect: "postgres",
-      }
-);
+//         dialectOptions: {
+//           ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//           },
+//         },
+//       }
+//     : {
+//         dialect: "postgres",
+//       }
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  host: !localhost,
+  dialect: "postgres",
+  ssl: true,
+});
 
 async function syncDb(sequelize, options) {
   const { force, alter } = options;
