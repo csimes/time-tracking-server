7b623ead9b16d79b94c94005e43cf871805f90d1
Cherron Simes <cherron@developdbycherron.com>
2022-12-11 19:13:38 -0600

    Refactor user controller amd middleware

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const router = require("express").Router();
 const { User } = require("../models");
 const { UniqueConstraintError } = require("sequelize/lib/errors");
@@ -5,77 +6,93 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
-  let {email, password, isAdmin} = req.body;
-    try {
+  try {
+    const { email, password, isAdmin } = req.body;
+    encryptedPassword = await bcrypt.hash(password, 13);
 
-			const user = await User.create({
-        email,
-        password: bcrypt.hashSync(password, 13),
-        isAdmin
-            });
-        let token = jwt.sign({ id: User.id }, process.env.JWT_SECRET, {
-            expiresIn: "1d",
-        });
-        res.status(201).json({
-            message: "User has been successfully registered!",
-            user: user,
-            sessionToken: token,
-        });
-    } catch (err) {
-        if (err instanceof UniqueConstraintError) {
-            res.status(409).json({
-                message: "Email already in use",
-            });
-        } else {
-            res.status(500).json({
-                message: `Unable to register user. ${err}`,
-            });
-        }
+    const user = await User.create({
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+      isAdmin,
+    });
+    const token = jwt.sign({ id: user.id, email }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
+
+    user.token = token;
+
+    res.status(201).json({
+      message: "User has been successfully registered!",
+      user: user,
+      sessionToken: token,
+    });
+  } catch (err) {
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
     }
+
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      res.status(409).json({
+        message: "Email already in use",
+      });
+    } else {
+      res.status(500).json({
+        message: `Unable to register user. ${err}`,
+      });
+    }
+  }
 });
 
 router.post("/login", async (req, res) => {
-    let { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-    try {
-        const loginUser = await User.findOne({
-            where: {
-                email,
-            },
-        });
-        if (loginUser) {
-            let passwordComparison = await bcrypt.compare(
-                password,
-                loginUser.password
-            );
-
-            if (passwordComparison) {
-                let token = jwt.sign(
-                    { id: loginUser.id },
-                    process.env.JWT_SECRET,
-                    { expiresIn: "1d" }
-                );
-
-                res.status(200).json({
-                    user: loginUser,
-                    message: "User successfully logged in!",
-                    sessionToken: token,
-                });
-            } else {
-                res.status(401).json({
-                    message: "Incorrect email or password",
-                });
-            }
-        } else {
-            res.status(401).json({
-                message: "Login failed",
-            });
-        }
-    } catch (err) {
-        res.status(500).json({
-            message: `Unable to log user in ${err}`
-        });
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
     }
+
+    const loginUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (loginUser && (await bcrypt.compare(password, loginUser.password))) {
+      //create user token
+
+      const token = jwt.sign(
+        { id: loginUser.id, email },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      loginUser.token = token;
+
+      res.status(200).json({
+        loginUser,
+        message: "User successfully logged in!",
+        sessionToken: token,
+      });
+      {
+        res.status(400).json({
+          message: "Incorrect email or password",
+        });
+      }
+    } else {
+      res.status(401).json({
+        message: "Login failed",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: `Unable to log user in. ${err}`,
+    });
+  }
 });
 
 module.exports = router;
