1214fac9603e1dd8a36f59fe7d42c5c58b6fb64b
Cherron Simes <cherron@developdbycherron.com>
2022-12-11 19:11:35 -0600

    Update user schema to include token as property

--- models/User.js
+++ models/User.js
@@ -1,18 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define("User", {
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        isAdmin: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-    });
-    return User;
+  const User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    token: {
+      type: DataTypes.STRING,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
+  return User;
 };
