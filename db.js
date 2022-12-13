a1659135f3c1edd16f7a6f140d09d454f13b51bb
Cherron Simes <cherron@developdbycherron.com>
2022-12-12 20:24:30 -0600

    Revert db to previous setup

--- db.js
+++ db.js
@@ -1,26 +1,24 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */
-//   `${process.env.DATABASE_URL}?ssl=true`,
-//   process.env.HOST != "localhost"
-//     ? {
-//         dialect: "postgres",
+const sequelize = new Sequelize(
+  /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */
+  process.env.DATABASE_URL,
+  process.env.HOST != "localhost"
+    ? {
+        dialect: "postgres",
 
-//         dialectOptions: {
-//           ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//           },
-//         },
-//       }
-//     : {
-//         dialect: "postgres",
-//       }
-// );
-
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}?ssl=true`);
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {
+        dialect: "postgres",
+      }
+);
 
 async function syncDb(sequelize, options) {
   const { force, alter } = options;
