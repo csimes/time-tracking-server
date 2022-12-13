20d3fe8b35e340851d8dc9ab0f06859b506e12c3
Cherron Simes <cherron@developdbycherron.com>
2022-12-12 22:14:49 -0600

    Trying old db connection setup

--- db.js
+++ db.js
@@ -1,33 +1,33 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */
-//   process.env.DATABASE_URL,
-//   process.env.HOST != "localhost"
-// ? {
-//         dialect: "postgres",
-
-//         dialectOptions: {
-//           ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//           },
-//         },
-//       }
-//     : {
-//         dialect: "postgres",
-//       }
-// );
 const sequelize = new Sequelize(
-  process.env.DB,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.HOST,
-    dialect: 'postgres',
-  }
+  /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */
+  process.env.DATABASE_URL,
+  process.env.HOST != "localhost"
+    ? {
+        dialect: "postgres",
+
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {
+        dialect: "postgres",
+      }
 );
+// const sequelize = new Sequelize(
+//   process.env.DB,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.HOST,
+//     dialect: 'postgres',
+//   }
+// );
 async function syncDb(sequelize, options) {
   const { force, alter } = options;
   try {
