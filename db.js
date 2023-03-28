db2342350273898136363453ca7bca065f8baed8
Cherron Simes <cherron@developdbycherron.com>
2023-03-27 21:26:17 -0500

    complete server setup with supabase

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize-cockroachdb");
 
 const sequelize = new Sequelize(
   /* ssl=true must be appended to database url to prevent error stating ssl/tls is required */
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
         dialectOptions: {
           ssl: {
             require: true,
-            rejectUnauthorized: false,
+            rejectUnauthorized: true,
           },
         },
       }
