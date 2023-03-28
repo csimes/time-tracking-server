54ef988b6a7f7fd475685f45360b6d13be08ffce
Cherron Simes <cherron@developdbycherron.com>
2023-03-27 21:49:37 -0500

    change data type to help supabase database populate

--- models/User.js
+++ models/User.js
@@ -1,16 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     email: {
-      type: DataTypes.STRING,
+      type: DataTypes.CHAR(100),
       allowNull: false,
       unique: true,
     },
     password: {
-      type: DataTypes.STRING,
+      type: DataTypes.CHAR(100),
       allowNull: false,
     },
     token: {
-      type: DataTypes.STRING,
+      type: DataTypes.CHAR(100),
     },
     isAdmin: {
       type: DataTypes.BOOLEAN,
