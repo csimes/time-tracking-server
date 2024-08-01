52ec863be17992b9ea796f879856561d5ad9b6f3
csimes <cherron@developdbycherron.com>
2024-07-31 23:21:02 -0500

    add basic testing setup and file scaffolding

---
+++ tests/setup.js
@@ -0,0 +1,14 @@
+// __tests__/User.test.js
+const { query } = require("./setup");
+
+describe("User Model Tests", () => {
+  it("should create a project", async () => {
+    // Test user creation
+  });
+
+  it("should retrieve a user", async () => {
+    // Test user retrieval
+  });
+
+  // More user-specific tests...
+});
